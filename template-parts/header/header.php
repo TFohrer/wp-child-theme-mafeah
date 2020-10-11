4f002f127b68e2fb09f521e54220372f90eaa1f9
TFohrer <t.fohrer@gmx.de>
2020-10-11 19:20:32 +0200

    update styling for header

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -33,8 +33,8 @@ $show_sticky = in_array($header_behavior, ['sticky-header-on-scroll-up', 'sticky
         <?php creator_elated_get_right_menu(); ?>
 
         <div class="header__shop-icons">
-            <?php the_widget('eltd_woocommerce_dropdown_cart'); ?>
-            
+            <?php the_widget('woocommerce-dropdown-cart'); ?>
+
             <div class="header-shop-icons__account">
                 <a href="/my-account"><i class="fa fa-user"></i></a>
             </div>
